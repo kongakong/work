456b599c929ddd47ed1da60be43a8d5110da4d11
akong <anthony.hw.kong@gmail.com>
2023-02-13 01:23:02 +1100

    Add more holidays

--- wfh_days/print_wfh_day_2021_2022.py
+++ wfh_days/print_wfh_day_2021_2022.py
@@ -4,26 +4,67 @@ from icalendar import Calendar, Event
 
 
 pub_2022_raw_data = [
-("New Year's Day",    "Sunday", "1 January"),
-("Additional Day",    "Tuesday", "3 January"),
-("Australia Day",    "Thursday", "26 January"),
-("Good Friday",    "Saturday", "15 April"),
-("Easter Saturday - the Saturday following Good Friday","Sunday", "16 April"),
-("Easter Sunday",    "Monday", "17 April"),
-("Easter Monday",    "Tuesday", "18 April"),
-("Anzac Day",    "Tuesday", "25 April"),
-("King's Birthday",    "Tuesday", "13 June"),
-("1 Bank Holiday",    "Tuesday", "1 August"),
-("National Day of Mourning",    "Friday", "22 September"),
-("Labour Day",    "Tuesday", "3 October"),
-("Christmas Day public holiday",    "Monday", "25 December"),
-("Additional Day",    "Wednesday", "27 December"),
-("Boxing Day",    "Tuesday", "26 December"),
+    ("New Year's Day", "Sunday", "1 January"),
+    ("Additional Day", "Tuesday", "3 January"),
+    ("Australia Day", "Thursday", "26 January"),
+    ("Good Friday", "Saturday", "15 April"),
+    ("Easter Saturday - the Saturday following Good Friday", "Sunday", "16 April"),
+    ("Easter Sunday", "Monday", "17 April"),
+    ("Easter Monday", "Tuesday", "18 April"),
+    ("Anzac Day", "Tuesday", "25 April"),
+    ("King's Birthday", "Tuesday", "13 June"),
+    ("1 Bank Holiday", "Tuesday", "1 August"),
+    ("National Day of Mourning", "Friday", "22 September"),
+    ("Labour Day", "Tuesday", "3 October"),
+    ("Christmas Day public holiday", "Monday", "25 December"),
+    ("Additional Day", "Wednesday", "27 December"),
+    ("Boxing Day", "Tuesday", "26 December"),
 ]
 
+pub_2021_raw_data = [
+    ("New Year's Day", "Sunday", "1 January"),
+    ("Australia Day", "Thursday", "26 January"),
+    ("Good Friday	", "Friday", "2 April"),
+    ("Easter Saturday - the Saturday following Good Friday	", "Saturday", "3 April"),
+    ("Easter Sunday", "Sunday", "4 April"),
+    ("Easter Monday", "Monday,", "5 April"),
+    ("Anzac Day", "Tuesday,", "25 April"),
+    ("King's Birthday	", "Monday", "14 June"),
+    ("Bank Holiday	", "Monday", "2 August"),
+    ("Labour Day	", "Monday", "4 October"),
+    ("Christmas Day public holiday	", "Monday", "25 December"),
+    ("Boxing Day	", "Tuesday", "26 December"),
+]
+
+pub_2023_raw_data = [
+    ("New Year's Day", "Sunday", "1 January"),
+    ("Additional Day", "Monday", "2 January"),
+    ("Australia Day", "Thursday", "26 January"),
+    ("Good Friday	", "Friday", "7 April"),
+    ("Easter Saturday - the Saturday following Good Friday	", "Saturday", "8 April"),
+    ("Easter Sunday", "Sunday", "9 April"),
+    ("Easter Monday", "Monday,", "10 April"),
+    ("Anzac Day", "Tuesday,", "25 April"),
+    ("King's Birthday	", "Monday", "12 June"),
+    ("Bank Holiday	", "Monday", "7 August"),
+    ("Labour Day	", "Monday", "2 October"),
+    ("Christmas Day public holiday	", "Monday", "25 December"),
+    ("Boxing Day	", "Tuesday", "26 December"),
+]
 date_format = "%d %B %Y"
-pub_2022 = [datetime.strptime(date_string + " 2022", date_format) for (_, _, date_string) in pub_2022_raw_data]
-print(pub_2022)
+pub_2021 = [
+    datetime.strptime(date_string + " 2021", date_format)
+    for (_, _, date_string) in pub_2021_raw_data
+]
+pub_2022 = [
+    datetime.strptime(date_string + " 2022", date_format)
+    for (_, _, date_string) in pub_2022_raw_data
+]
+pub_2023 = [
+    datetime.strptime(date_string + " 2023", date_format)
+    for (_, _, date_string) in pub_2023_raw_data
+]
+
 
 def all_mondays(start, end):
     monday = timedelta(days=-6)
@@ -33,21 +74,25 @@ def all_mondays(start, end):
             yield d
         d += timedelta(days=1)
 
-start_date = datetime(2021, 1, 1, 0, 0)
-end_date = datetime(2022, 12, 31, 0, 0)
+
+start_date = datetime(2021, 7, 1, 0, 0)
+end_date = datetime(2022, 6, 30, 0, 0)
 mondays = list(all_mondays(start_date, end_date))
+
+all_holidays = pub_2021 + pub_2022
+print(all_holidays)
 
 cal = Calendar()
 for monday in mondays:
-    if monday in pub_2022: 
+    if monday in all_holidays:
         print("%s is a public holiday" % monday)
     else:
         event = Event()
-        event.add('summary', 'WFH Monday')
-        event.add('dtstart', monday)
-        event.add('dtend', monday + timedelta(days=1))
-        event.add('dtstamp', monday)
+        event.add("summary", "WFH Monday")
+        event.add("dtstart", monday)
+        event.add("dtend", monday + timedelta(days=1))
+        event.add("dtstamp", monday)
         cal.add_component(event)
 
-with open('mondays.ics', 'wb') as f:
+with open("mondays.ics", "wb") as f:
     f.write(cal.to_ical())
